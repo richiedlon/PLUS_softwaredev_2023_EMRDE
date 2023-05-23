3e880a4380cf00a805dc39a3492a871a902bbd11
Hannah <hannah.augustin@gmail.com>
2023-05-23 12:47:21 +0200

    Create A1_augustin.py

---
+++ A1_augustin.py
@@ -0,0 +1,29 @@
+"""A1
+Merge this pull request for full points. :)
+"""
+#
+#
+### Quote:
+#
+# "Attention without feeling is only a report" -- Mary Oliver
+#
+#
+### Disliked food/flavour:
+#
+# I can eat them, but raw green bell peppers taste kinda chemically and I try to avoid them.
+#
+### Recommenation:
+#
+# "Ways of Being: Animals, Plants, Machines: The Search for a Planetary Intelligence " by James Bridle
+#
+'''Another quote from the recommended book:
+“The logical conclusion of the development of wildlife corridors and protected areas -- even mobile ones -- is that there are places which must be left to non-humans, even in a more-than-human world.
+This is not a new suggestion. It goes back at least to the beginning of the twentieth century in Western culture, to the founding of National Parks in Europe and America, and it is intrinsic to non-Western conceptions of our place among the species of the planet. But there is growing awareness that it is now more urgent and must be much more extensive than a few scattered parks and sanctuaries. Indeed, there is a strong scientific and moral case that it should comprise at least half the entire Earth.”
+― James Bridle, Ways of Being: Animals, Plants, Machines: The Search for a Planetary Intelligence 
+'''
+#
+#
+### German word:
+#
+# "Mahlzeit" Austrian for "enjoy your meal", but also used as a greeting to say hello to anyone around lunchtime (11-14:00)
+#
